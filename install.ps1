fee94bd33499ded8a2dca9db67fef89e1dd293a7
northt <4067703+northt@users.noreply.github.com>
2022-01-10 22:17:49 +0900

    add install.ps1

---
+++ install.ps1
@@ -0,0 +1,55 @@
+iwr -useb get.scoop.sh | iex
+
+scoop install git
+scoop bucket add extras
+
+# CLI
+scoop install sudo
+scoop install jq
+scoop install unzip
+scoop install git-lfs
+scoop install ghq
+scoop install posh-git
+
+# Webブラウザ
+scoop install googlechrome
+
+# エディタ
+scoop install vscode
+scoop install neovim
+
+# ユーティリティ
+scoop install 7zip
+scoop install powertoys
+scoop install autohotkey
+scoop install screentogif
+scoop install slack
+
+# 画像編集
+scoop install gimp
+scoop install krita
+
+# 音楽・動画再生
+scoop install foobar2000
+scoop install mpc-be
+scoop install vlc
+
+# 開発系
+scoop install windows-terminal
+scoop install teraterm
+scoop install winscp
+scoop install winmerge
+scoop install sourcetree
+scoop install maven
+scoop install jetbrains-toolbox
+
+scoop install python
+scoop install go
+
+scoop bucket add java
+scoop install temurin8-jdk
+scoop install openjdk17
+scoop install kotlin
+
+scoop install ruby
+scoop install msys2
